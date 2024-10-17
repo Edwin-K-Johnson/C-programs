bbee7a1ba7e6782d6d414f11aa019aeddbc79ab8
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 23:22:46 +0530

    Update ASCII.c

--- ASCII.c
+++ ASCII.c
@@ -4,7 +4,7 @@ int main()
     char ch;
     printf("enter ch :");
 	scanf("%c",&ch);
-	printf("the ASCII value of %c id %d",ch,(int)ch);
+	printf("the ASCII value of %c iS %d",ch,(int)ch);
 	
     return 0;	
 }    
