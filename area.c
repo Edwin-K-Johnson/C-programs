3ca1705bcc7c3ca199678f31d9452f8ce1ea04ca
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 19:15:56 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int r,a,pi=3.14;
+    printf ("enter the radius");
+    scanf ("%d",&r);
+    a=pi*(r*r);
+	printf ("%d is area",a);
+	
+    return 0;
+}    
+
