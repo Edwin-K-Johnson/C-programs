7353198fdc33bd9980ad4766715aa4612f8196d4
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 11:43:26 +0530

    Add files via upload

---
+++ largestof2no..c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,l;
+    printf ("enter 2 no a and b");
+    scanf ("%d%d",&a,&b);
+    l=a>b?a:b;
+    printf("largest=%d",l);
+    return 0;
+}    
+
