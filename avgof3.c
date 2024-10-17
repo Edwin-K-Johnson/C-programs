7ae7132ca47388fe2fbc11a8646d4540b36331ed
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 12:09:33 +0530

    Add files via upload

---
+++ avgof3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c,average;
+    printf ("enter 3 no ");
+    scanf ("%f%f%f",&a,&b,&c);
+    average=(a+b+c)/3;
+    printf("the average is %f",average);
+    
+    return 0;
+}    
+
