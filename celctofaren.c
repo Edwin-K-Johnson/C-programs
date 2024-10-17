216473dccd95e4999ad53d1cfa011a539955491f
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 17:50:36 +0530

    Add files via upload

---
+++ celctofaren.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float c,f;
+    printf ("enter the celcius :");
+    scanf ("%f",&c);
+    f=(c*9/5)+32;
+    printf("the farenheit %f ",f);
+    
+    return 0;
+}    
+
