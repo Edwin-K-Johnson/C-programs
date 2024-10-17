d04293e5fab67653ece41837b5b2b424e2f5af85
Edwin-K-Johnson <iedwinkjohnson@gmail.com>
2024-10-17 22:16:07 +0530

    Add files via upload

---
+++ interchangeXOR.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	printf ("enter two numbers :");
+	scanf ("%d%d",&a,&b);
+	a=a^b;
+	b=a^b;
+	a=a^b;
+	printf ("the interchanged values are %d\t%d",a,b);
+	return 0;
+}
+
